e6631c9bc54b477aa3a976c4948625bcab8edf31
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-02 13:26:18 -0300

    LevelNoob

---
+++ ALevelNoob/constantes.js
@@ -0,0 +1,13 @@
+//Não podemos criar constantes com palavras reservadas
+//Constantes precisam ter nome significativos
+//Não pode começar o nome de uma constante com número
+//não pode conter espaaços e traços
+//como convenção utilizamos camelCase
+// case sensitive
+//Não utilize var sempre utilize CONST
+
+const NOME = 'Gustavo'
+
+const numero = 5;
+
+console.log(typeof(numero))  //para saber o tipo da variavel
---
+++ ALevelNoob/letevar.js
@@ -0,0 +1,13 @@
+//com var posso redeclarar uma variavel o LET não permite
+
+var nome = 'Ghjjjk';
+var nome = 'jhvjhjh';
+console.log(nome);
+
+let nome2 = 'kfgkjfgjf';
+//let nome2 = 'ddfdfdfd';
+console.log(nome2)
+
+
+//pode fazer mas não faça
+nome = "jhfjhfjh"; //declarar sem LET ou var ou const
