72fa0bff2d9a82521e2113e9f07abe22e06621e6
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-11-03 20:31:53 -0300

    Arithmetics

---
+++ COperators/OperatorsArithmetic.js
@@ -0,0 +1,11 @@
+/* Operadores aritmeticos
+        + (Adição / contatenação)
+        - (subtração)
+        / (divisão)
+        * (multiplicação)
+        ** (potenciação)
+        % (resto da divisão)
+
+        ++ (operador de incremento)
+        -- (operador de decremento)
+*/
